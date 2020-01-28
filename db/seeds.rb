8cf27d8f67eb322981978ad3b28640ddf31110b6
Khemraj Chauhan <khemraj@nestaway.com>
2020-01-28 23:47:16 +0530

    seed data for product desgin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#populating the initial product data
+items = ['A', 'B', 'C', 'D']
+prices = [30, 20, 50, 15]
+
+items.each_with_index do |item, index|
+  Product.where(item: item, price: prices[index]).first_or_create
+end
+
+discount_name = ['discount_on_item_A', 'discount_on_item_B']
+discount_prices = [75, 35]
+product_ids = Product.where(item: ['A', 'B']).pluck(:id)
+quantities = [3, 2]
+
+discount_name.each_with_index do |name, index|
+  discount = Discount.where(name: name, price: discount_prices[index], total_quantity: quantities[index])
+                     .first_or_create
+  product_discount = ProductDiscount.where(
+    product_id: product_ids[index], discount_id: discount.id, quantity: quantities[index]
+  ).first_or_create
+end
+
+GlobalDiscount.where(cart_min_value: 150, discount: 20).first_or_create
