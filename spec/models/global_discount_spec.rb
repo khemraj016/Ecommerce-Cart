b83f764562f615634430435d9aae5ac1d860db66
Khemraj Chauhan <khemraj@nestaway.com>
2020-01-30 11:17:26 +0530

    model spec

--- spec/models/global_discount_spec.rb
+++ spec/models/global_discount_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GlobalDiscount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:global_discount) { build_stubbed(:global_discount) }
+
+  describe '#cart_min_value' do
+    it { expect(global_discount).to validate_presence_of(:cart_min_value) }
+  end
+
+  describe '#discount' do
+    it { expect(global_discount).to validate_presence_of(:discount) }
+  end
+
+  describe '#cart_min_value' do
+    it { expect(global_discount).to have_db_column(:cart_min_value).of_type(:decimal) }
+  end
+
+  describe '#discount' do
+    it { expect(global_discount).to have_db_column(:discount).of_type(:decimal) }
+  end
+
+  describe '#status' do
+    it { expect(global_discount).to have_db_column(:status).of_type(:string)
+                                                           .with_options(default: 'active') }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build_stubbed(:user) }
+
+  describe '#email' do
+    it { expect(user).to have_db_column(:email).of_type(:string) }
+  end
+
+  describe '#guest' do
+    it { expect(user).to have_db_column(:guest).of_type(:boolean) }
+  end
 end
