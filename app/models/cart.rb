c607f48c8ed214b0460bd498cf0ab473077d13b1
Khemraj Chauhan <khemraj@nestaway.com>
2020-01-30 11:14:27 +0530

    model validation added

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  validates :quantity, presence: true
+
   enum statuses: {active: 'active', inactive: 'inactive'}
 
   validates :status, inclusion: { in: Cart.statuses.values }
