7320ccfc7e68743b63be980ad35f96d7f53b30b5
Khemraj Chauhan <khemraj@nestaway.com>
2020-01-31 23:04:53 +0530

    javascript moved from index erb to js file

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,103 +1,61 @@
-$(document).ready(function() {
-  var fadeTime = 300;
- 
- 
-  /* Assign actions */
-  $('.product-quantity input').change( function() {
-    updateQuantity(this);
-  });
-   
-  $('.product-removal button').click( function() {
-    removeItem(this);
-  });
- 
- 
-  /* Recalculate cart */
-  function recalculateCart()
-  {
-    var subtotal = 0;
-     
-    /* Sum up row totals */
-    $('.product').each(function () {
-      subtotal += parseFloat($(this).children('.product-line-price').text());
-    });
-     
-    /* Calculate totals */
-    // var tax = subtotal * taxRate;
-    // var shipping = (subtotal > 0 ? shippingRate : 0);
-    additionalDiscount(subtotal);
-    var total = subtotal; //+ shipping;
-     
-    /* Update totals display */
-    $('.totals-value').fadeOut(fadeTime, function() {
-      $('#cart-subtotal').html(subtotal.toFixed(2));
-      // $('#additial-discount').html(discount_on_total.toFixed(2));
-      // $('#cart-shipping').html(shipping.toFixed(2));
-      total_after_discount = total - parseFloat($('#discount').text()) - parseFloat($('#additial-discount').text());
-      $('#cart-total').html(total_after_discount.toFixed(2));
-      if(total == 0){
-        $('.checkout').fadeOut(fadeTime);
-      }else{
-        $('.checkout').fadeIn(fadeTime);
-      }
-      $('.totals-value').fadeIn(fadeTime);
-    });
-  }
-
-  function additionalDiscount(total_price)
-  {
-    var product_id = document.getElementById("product_id").value
-    var quantity = $('.product-quantity input').val();
-    discount_text = $('.product-line-price').text();
-    var previous_discount = parseFloat(discount_text.match(/[0-9]+/)[0], 10) - parseFloat($('#discount').text());
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:3000/products/cart",
-      data: JSON.stringify({product: {'id': product_id, 'quantity': quantity, 'total_price': total_price}}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(response) {
-        if(response.success == true){
-          // $('#additial-discount').html(response['data']['global_discount']);
-          // if(response['data']['discount'] != 0) {
-          //   $('#discount').html(total_price - response['data']['discount']);
-          // }
-        }
-        else {
-          alert('Error');
-        }
-      }
-    });
-  }
- 
-  /* Update quantity */
-  function updateQuantity(quantityInput)
-  {
-    /* Calculate line price */
-    var productRow = $(quantityInput).parent().parent();
-    var price = parseFloat(productRow.children('.product-price').text());
-    var quantity = $(quantityInput).val();
-    var linePrice = price * quantity;
-    /* Update line price display and recalc cart totals */
-    productRow.children('.product-line-price').each(function () {
-      $(this).fadeOut(fadeTime, function() {
-        $(this).text(linePrice.toFixed(2));
-        recalculateCart();
-        $(this).fadeIn(fadeTime);
+function addToCart(product_id) {
+  document.getElementById("final_cart").style.display = "none";
+  var quantity = $(".product-quantity input#qt-"+ product_id).val();
+  document.getElementById("btn-" + product_id).disabled = true;
+  
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3000/products/cart",
+    data: JSON.stringify({product: {'id': product_id, 'quantity': quantity}}),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(response) {
+      // alert('Item successfully added to your cart');
+      $('#flash').delay(500).fadeIn('normal', function() {
+        $(this).delay(2500).fadeOut();
       });
-    });  
-  }
- 
-  /* Remove item from cart */
-  function removeItem(removeButton)
-  {
-    /* Remove row from DOM and recalc cart total */
-    var productRow = $(removeButton).parent().parent();
-    productRow.slideUp(fadeTime, function() {
-      productRow.remove();
-      recalculateCart();
-    });
-  }
+      document.getElementById("btn-" + product_id).disabled = false;
+    }
+  });
+}
 
-});
- 
+function paymentDetails() {
+  var fadeTime = 300;
+  var subtotal;
+  var discount_price;
+  var additial_discount;
+  var cart_total;
+  
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/products/calculate",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(response) {
+      if(response.success) {
+        $('#cart-subtotal').html(response['data']['cart_price']);
+        $('#discount').html(response['data']['discount_price']);
+        $('#additial-discount').html(response['data']['additional_discount']);
+        $('#cart-total').html(response['data']['total']);
+        document.getElementById("final_cart").style.display = "block";
+      }
+    }
+  });
+}
+
+function clearCart() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/products/cart_clear",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(response) {
+    }
+  });
+}
+
+if (performance.navigation.type == 1) {
+  clearCart()
+}
+
+
